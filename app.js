d6d455cd7663b279851570d5d767274b6152823e
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-01-29 10:04:36 +0530

    Migrated the server from net module to http module

--- app.js
+++ app.js
@@ -1,58 +1,75 @@
-const {readFileSync, existsSync, statSync, writeFileSync} = require('fs');
-const Response = require('./lib/response');
+const { readFileSync, existsSync, statSync, writeFileSync } = require('fs');
 const CONTENT_TYPES = require('./lib/mimeTypes');
-const {loadTemplate} = require('./lib/viewTemplates');
+const { loadTemplate } = require('./lib/viewTemplates');
 
 const STATIC_DIR = `${__dirname}/public`;
 const STORAGE_FILE = `${__dirname}/commentInfo.json`;
 
-const serveHomePage = request => {
+const serveHomePage = (request, response) => {
   request.url = '/index.html';
-  return serveStaticFile(request);
-}
+  return serveStaticFile(request, response);
+};
 
 const isFileValid = (path) => {
   const status = existsSync(path) && statSync(path);
   return status || status.isFile;
-}
+};
 
-const generateOkResponse = function(contentType,content){
-  const response = new Response();
+const generateOkResponse = function (contentType, content, response) {
   response.statusCode = 200;
   response.setHeader('Content-Type', contentType);
   response.setHeader('Content-Length', content.length);
-  response.body = content;
-  return response;
-}
-
-const serveStaticFile = function(request){
-  const filePath = `${STATIC_DIR}${request.url}`
-	if(!isFileValid(filePath)) return new Response();
-  const [,extension] = request.url.match(/.*\.(.*)$/);
-  const content = readFileSync(filePath);
-  const contentType = CONTENT_TYPES[extension];
-  return generateOkResponse(contentType,content);
+  return response.end(content);
 };
 
-const generateCommitInfo = ({usrName, comment}) => {
-  return {date: new Date().toJSON(), usrName, comment};
-}
+const serveStaticFile = function (request, response) {
+  const filePath = `${STATIC_DIR}${request.url}`
+  if (!isFileValid(filePath)) return serveNotFoundResponse(request, response);
+  const [, extension] = request.url.match(/.*\.(.*)$/);
+  const content = readFileSync(filePath);
+  const contentType = CONTENT_TYPES[extension];
+  return generateOkResponse(contentType, content, response);
+};
+
+const generateCommitInfo = (body) => {
+  const { usrName, comment } = organizeQueryParams(body)
+  return { date: new Date().toJSON(), usrName, comment };
+};
 
 const loadComments = () => {
-  if(!existsSync(STORAGE_FILE)) return [];
+  if (!existsSync(STORAGE_FILE)) return [];
   return JSON.parse(readFileSync(STORAGE_FILE, 'utf8'));
-}
+};
 
-const updateComment = function(req) {
-  const storedComments = loadComments();
-  const newComment = generateCommitInfo(req.body);
-  storedComments.unshift(newComment);
-  const commentsInfoString = JSON.stringify(storedComments)
-  writeFileSync(STORAGE_FILE, commentsInfoString);
-  return serveGuestPage(req)  
-}
+const decodeValue = (text) => {
+  const decoded = decodeURIComponent(text);
+  let value = decoded.replace(/\+/g, ' ');
+  return value;
+};
 
-const formateSingleComment = function(comment){
+const pickupParams = (keyValuePairs, keyValue) => {
+  const [key, value] = keyValue.split('=');
+  keyValuePairs[key] = decodeValue(value);
+  return keyValuePairs;
+};
+
+const organizeQueryParams = (keyValuePairs) => keyValuePairs.split('&').reduce(pickupParams, {});
+
+const updateComment = function (req, res) {
+  let body = '';
+  req.on('data', data => body += data);
+  req.on('end', () => {
+    req.query = organizeQueryParams(body);
+    const storedComments = loadComments();
+    const newComment = generateCommitInfo(body);
+    storedComments.unshift(newComment);
+    const commentsInfoString = JSON.stringify(storedComments)
+    writeFileSync(STORAGE_FILE, commentsInfoString);
+    return serveGuestPage(req, res)
+  });
+};
+
+const formateSingleComment = function (comment) {
   const date = new Date(comment.date);
   return `<div class="guestCommentBox">
       <span class="cmtGuestName">${comment.usrName}</span>
@@ -62,27 +79,35 @@ const formateSingleComment = function(comment){
 };
 
 const formateComments = (comments) => {
-  return comments.reduce((formattedCom, comment)=> 
-  `${formattedCom}\n ${formateSingleComment(comment)} </br>`,'')};
-
-const serveGuestPage = function(req) {
-  const comments = loadComments();
-  const formattedComments = formateComments(comments);
-  const html = loadTemplate(req.url, {'comments' : formattedComments}); 
-  return generateOkResponse(CONTENT_TYPES.html, html);
-}
-
-const findHandle = function(req){
-  if(req.method === 'GET' && req.url === '/') return serveHomePage;
-  if(req.method === 'GET' && req.url === '/guestBook.html') return serveGuestPage;
-  if(req.method === 'GET') return serveStaticFile;
-  if(req.method === 'POST' && req.url === '/guestBook.html') return updateComment;
-	return ()=> new Response();
+  return comments.reduce((formattedCom, comment) =>
+    `${formattedCom}\n ${formateSingleComment(comment)} </br>`, '')
 };
 
-const processRequest = function(request){
-  const handler = findHandle(request);
-  return handler(request);
+const serveGuestPage = function (req, response) {
+  const comments = loadComments();
+  const formattedComments = formateComments(comments);
+  const html = loadTemplate(req.url, { 'comments': formattedComments });
+  return generateOkResponse(CONTENT_TYPES.html, html, response);
+};
+
+const serveNotFoundResponse = (request, response) => {
+  response.statusCode = 404;
+  response.setHeader('Content-Length', 0);
+  return response.end();
 }
 
-module.exports = { processRequest};
+const findHandle = function (req) {
+  if (req.method === 'GET' && req.url === '/') return serveHomePage;
+  if (req.method === 'GET' && req.url === '/guestBook.html') return serveGuestPage;
+  if (req.method === 'GET') return serveStaticFile;
+  if (req.method === 'POST' && req.url === '/guestBook.html') return updateComment;
+  return serveNotFoundResponse;
+};
+
+const processRequest = function (request, response) {
+  console.warn(`Request: {method: ${request.method}, url :${request.url}}\n`);
+  const handler = findHandle(request);
+  return handler(request, response);
+};
+
+module.exports = { processRequest };
--- server.js
+++ server.js
@@ -1,31 +1,19 @@
-const { Server } = require('net');
-const Request = require('./lib/request');
-const {processRequest} = require('./app');
+const http = require('http');
+const { processRequest } = require('./app');
 
-const handleRequestText = (text, socket) => {
-  console.warn(`data: ${text}`);
-	const req = Request.parse(text); 
-  const response = processRequest(req);
-	response.write(socket);  
-};
-
-const handleConnection = socket => {
-	const remote = `{ ${socket.remoteAddress} : ${socket.remotePort}}`;
+const handleConnection = (request, response) => {
+	request.setEncoding('utf8');
+	const { address, port } = request.socket.address();
+	const remote = `{ address: ${address}, port: ${port}}`;
 	console.warn(`Connected to ${remote}`);
-	socket.setEncoding('utf8');
-	socket.on('data', (text) => handleRequestText(text, socket));
-	socket.on('error', (err) => console.warn('Error', err))
-	socket.on('close', (err) => console.warn(`${remote} closed ${err ? 'with error' : ''}`));
-	socket.on('end', () => console.warn(`${remote} ended`)); 
-	socket.on('drain', () => console.warn(`${remote} drained`));
+	processRequest(request, response);
 };
 
-const main = function(port) {
-	const server = new Server();
-	server.on('connection', handleConnection);
+const main = function (port) {
+	const server = new http.Server(handleConnection);
 	server.on('error', (error) => console.error('------Sever Error: ', error.message));
-	server.listen(port,() => console.warn(`Serving HTTP on 0.0.0.0 port ${port} (http://0.0.0.0:${port}/) ...`)
- )
+	server.listen(port, () => console.warn(`Serving HTTP on 0.0.0.0 port ${port} (http://0.0.0.0:${port}/) ...`)
+	)
 };
 
 main(8000);
