f3685e5bc72eede07695d2758d4a8cf9ca983f30
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-01-30 20:01:33 +0530

    Added more test cases for the GET method

--- test/handlersTest.js
+++ test/handlersTest.js
@@ -1,13 +1,45 @@
 const request = require('supertest');
 const {app} = require('./../lib/handlers');
-
-describe('GET /', () => {
-  it('Should give the home page', (done) => { 
-    request(app.serve.bind(app))
-      .get('/')
-      .set('Accept', '*/*')
-      .expect(200)
-      .expect('Content-Type', 'text/html')
-      .expect('Content-Length', '796', done);
+describe('GET', () => {
+  describe('URL: /', () => {
+    it('Should give the home page', (done) => { 
+      request(app.serve.bind(app))
+        .get('/')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', 'text/html')
+        .expect('Content-Length', '796', done);
+    });
   });
+
+  describe('URL: /guestBook.html', () => {
+    it('Should give the guest book page', (done) => { 
+      request(app.serve.bind(app))
+        .get('/guestBook.html')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Type', 'text/html', done);
+    });
+  });
+
+  describe('URL: /abeliophyllum.html', () => {
+    it('Should give the flower content page', (done) => { 
+      request(app.serve.bind(app))
+        .get('/abeliophyllum.html')
+        .set('Accept', '*/*')
+        .expect(200)
+        .expect('Content-Length', '1522')
+        .expect('Content-Type', 'text/html', done);
+    });
+  });
+
+  describe('URL: /badFile', () => {
+    it('Should give error 404 when the url is not exists', (done) => { 
+      request(app.serve.bind(app))
+        .get('/badFile')
+        .set('Accept', '*/*')
+        .expect(404, done);
+    });
+  });
+
 });
