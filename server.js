26e0fd68829d1e47bf7b13cb38ceb2a56111708c
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-01-30 10:22:19 +0530

    Added the App class which deals with routing

--- server.js
+++ server.js
@@ -1,26 +1,8 @@
 const http = require('http');
-const { methodHandlers } = require('./lib/handlers');
-
-const matchedHandlers = function (request, router) {
-	return request.url.match(router.path);
-}
-
-const processRequest = function (request, response) {
-	const { address, port } = request.socket.address();
-	const remote = `{ address: ${address}, port: ${port}}`;
-	console.warn(`Connected to ${remote}`);
-
-	const routers = methodHandlers[request.method] || methodHandlers.notFount;
-	const matchingHandlers = routers.filter((router) => matchedHandlers(request, router));
-	const next = () => {
-		const router = matchingHandlers.shift();
-		router.handler(request, response);
-	};
-	next();
-};
+const { app } = require('./lib/handlers');
 
 const main = function (port) {
-	const server = new http.Server(processRequest);
+	const server = new http.Server(app.serve.bind(app));
 	server.on('error', (error) => console.error('------Sever Error: ', error.message));
 	server.listen(port, () => console.warn(`Serving HTTP on 0.0.0.0 port ${port} (http://0.0.0.0:${port}/) ...`)
 	)
