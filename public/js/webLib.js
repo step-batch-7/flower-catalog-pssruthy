10620b48fc2ba83dc3afdcddae3da3139a8c3956
Sruthy P S <58027046+pssruthy@users.noreply.github.com>
2020-01-30 11:26:00 +0530

    Added eslit and modified the code
    * overrides public/js/webLib.js file from no-unused-vars
      and no-undef.

--- public/js/webLib.js
+++ public/js/webLib.js
@@ -1,6 +1,9 @@
 const hideForOneSec = function () {
   const jar = document.getElementById('jarGIF');
   jar.style.visibility = 'hidden';
+  const timeToHIde = 1000;
 
-  setTimeout(() => jar.style.visibility = 'visible', 1000);
-}
+  setTimeout(() => {
+    jar.style.visibility = 'visible';
+  }, timeToHIde);
+};
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const http = require('http');
-const { app } = require('./lib/handlers');
+const {app} = require('./lib/handlers');
+const {stderr, stdout} = process; 
 
 const main = function (port) {
-	const server = new http.Server(app.serve.bind(app));
-	server.on('error', (error) => console.error('------Sever Error: ', error.message));
-	server.listen(port, () => console.warn(`Serving HTTP on 0.0.0.0 port ${port} (http://0.0.0.0:${port}/) ...`)
-	)
+  const server = new http.Server(app.serve.bind(app));
+  server.on('error', (error) => {
+    stderr.write('------Sever Error: ', error.message);
+  });
+  server.listen(port, () => {
+    stdout.write(`Serving HTTP on port ${port} (http://0.0.0.0:${port}/)`);
+  });
 };
-
-main(8000);
+const port = 8000;
+main(port);
